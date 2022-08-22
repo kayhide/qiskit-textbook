1b7f22288de87a28ce41d148d2a96322ebc045ca
Hideaki Kawai <kayhide@gmail.com>
2022-08-22 12:01:48 +0900

    Update dev shell

--- flake.nix
+++ flake.nix
@@ -6,26 +6,50 @@
 
   outputs = inputs:
     let
-      overlay = final: prev: {
-        my-python = final.python3;
-      };
+      overlay = final: prev: { };
       perSystem = system:
         let
           pkgs = import inputs.nixpkgs { inherit system; overlays = [ overlay ]; };
 
+          my-python = pkgs.python3.withPackages (p: with p; [
+            pip
+            setuptools
+            virtualenv
+            wheel
+          ]);
+
           lib-path = with pkgs; lib.makeLibraryPath [
             stdenv.cc.cc
           ];
+
+          dev-env = pkgs.buildEnv {
+            name = "dev-env";
+            paths = with pkgs; [
+              my-python
+              my-python.pkgs.pip
+              poetry
+              gnumake
+            ];
+
+            # nativeBuildInputs = [
+            #   pkgs.makeWrapper
+            # ];
+
+            # postBuild = ''
+            #   wrapProgram $out/bin/python \
+            #     --set LD_LIBRARY_PATH ${lib-path}
+            #   wrapProgram $out/bin/poetry \
+            #     --set LD_LIBRARY_PATH ${lib-path}
+            # '';
+          };
         in
         {
           devShell = pkgs.mkShell {
             buildInputs = with pkgs; [
-              my-python
-              poetry
-              gnumake
+              dev-env
             ];
-            LD_LIBRARY_PATH = lib-path;
 
+            LD_LIBRARY_PATH = lib-path;
           };
         };
     in
